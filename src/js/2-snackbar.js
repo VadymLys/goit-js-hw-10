c43c4f01a0fc7e06b7f280bd9b07956d94c037de
VadymLys <144118540+VadymLys@users.noreply.github.com>
2024-02-03 09:08:57 +0100

    'update'

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,33 @@
+import iziToast from "izitoast";
+import 'iziToast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const delay = form.delay.value;
+    const state = form.state.value;
+    const promise = new Promise(resolve, reject)
+    if (state === 'fullfield') {
+        setTimeout(() => resolve(delay), delay);
+    } else if (state === 'rejected') {
+        setTimeout(() => reject(delay), delay);
+    }
+
+});
+
+promise.then((delay) => {
+    iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'bottomCenter',
+    });
+})
+    .catch((delay) => {
+        iziToast.error({
+        message: `❌Rejected promise in ${delay}ms`,
+        position: 'bottomCenter',
+    })
+    })
+
+form.addEventListener('submit', createPromise);
